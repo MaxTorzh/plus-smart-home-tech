84f567e42d8df13b02313008d6656e2303ba2c91
Maksim Torzhkov <maxtorj@yandex.ru>
2025-12-16 22:14:19 +0300

    feat: update project structure

--- telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/exception/DeserializationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.kafka.exception;
-
-public class DeserializationException extends RuntimeException {
-    public DeserializationException(String message, Throwable e) {
-        super(message);
-    }
-}
