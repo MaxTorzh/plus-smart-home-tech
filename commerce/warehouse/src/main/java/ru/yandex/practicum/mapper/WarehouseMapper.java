73915ff0e78142b250ae6d5a26a44fad828d8d05
Maksim Torzhkov <maxtorj@yandex.ru>
2026-01-14 14:40:49 +0300

    feat: update warehouse environment

--- commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
@@ -2,10 +2,15 @@ package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 import ru.yandex.practicum.model.WarehouseProduct;
 import ru.yandex.practicum.dto.NewProductInWarehouse;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface WarehouseMapper {
+
     WarehouseProduct toWarehouse(NewProductInWarehouse request);
 }
