e18564b8df7f30ee7f2d06c853056638cb211bf0
Maksim Torzhkov <maxtorj@yandex.ru>
2026-01-14 15:09:09 +0300

    feat: update poms for commerce module

--- commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
@@ -2,15 +2,10 @@ package ru.yandex.practicum.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import org.mapstruct.ReportingPolicy;
 import ru.yandex.practicum.model.WarehouseProduct;
 import ru.yandex.practicum.dto.NewProductInWarehouse;
 
-@Mapper(
-        componentModel = MappingConstants.ComponentModel.SPRING,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE
-)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface WarehouseMapper {
-
     WarehouseProduct toWarehouse(NewProductInWarehouse request);
 }
