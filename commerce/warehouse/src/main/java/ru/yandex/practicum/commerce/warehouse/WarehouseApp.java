01411974688946d7f709ac2c23963d8797482f4c
Maksim Torzhkov <maxtorj@yandex.ru>
2026-01-15 12:54:48 +0300

    feat: warehouse refactoring

---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/commerce/warehouse/WarehouseApp.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.commerce.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "ru.yandex.practicum.commerce.warehouse",
+        "ru.yandex.practicum.exception.handler"
+})
+public class WarehouseApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApp.class,args);
+    }
+}
