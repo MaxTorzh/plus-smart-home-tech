27f3be4775597703e69eddfec9c8af7296b466a4
Maksim Torzhkov <maxtorj@yandex.ru>
2026-01-15 14:52:38 +0300

    feat: update exception handler

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/exception/handler/ShoppingStoreExceptionHandler.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/exception/handler/ShoppingStoreExceptionHandler.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.exception.handler;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "ru.yandex.practicum.commerce.store")
+@RestControllerAdvice
 public class ShoppingStoreExceptionHandler extends GlobalExceptionHandler {
 }
--- commerce/interaction-api/src/main/java/ru/yandex/practicum/exception/handler/WarehouseExceptionHandler.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/exception/handler/WarehouseExceptionHandler.java
@@ -2,6 +2,6 @@ package ru.yandex.practicum.exception.handler;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "ru.yandex.practicum.commerce.warehouse")
+@RestControllerAdvice
 public class WarehouseExceptionHandler extends GlobalExceptionHandler {
 }
